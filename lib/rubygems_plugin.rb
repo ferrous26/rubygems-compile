6ebe02df7afd9e66f0ac27715f82c50a82eca455
Mark Rada <mrada@marketcircle.com>
2011-02-16 00:05:07 -0500

    A working post install compilation step

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,11 +1,14 @@
 Gem.post_install do |gem|
 
   spec  = gem.spec
+  dir   = gem.gem_home + '/gems/' + spec.name + '-' + spec.version.version
   files = spec.files - spec.test_files - spec.extra_rdoc_files
 
   files.each do |file|
     next unless file.match /\.rb$/
     puts "Compiling #{file} to #{file}o"
+    file = "#{dir}/#{file}"
+    `macrubyc --verbose --arch x86_64 -C '#{file}' -o '#{file}o'`
   end
 
 end
