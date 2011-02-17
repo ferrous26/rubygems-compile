021c95ef869035cd001d1ebd1d44e5e399f80420
Mark Rada <mrada@marketcircle.com>
2011-02-17 01:56:06 -0500

    Try harder to ignore test files

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,7 +2,9 @@ Gem.post_install do |gem|
 
   spec  = gem.spec
   dir   = gem.gem_home + '/gems/' + spec.name + '-' + spec.version.version
+
   files = spec.files - spec.test_files - spec.extra_rdoc_files
+  files = files.reject { |file| file.match /^(?:test|spec)/ }
 
   files.each do |file|
     next unless file.match /\.rb$/
