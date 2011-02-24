9257d49438974223b15649ed77ee744363ec7011
Mark Rada <mrada@marketcircle.com>
2011-02-24 02:42:28 -0500

    First iteration of the compile command
    It needs tests, some prettying up, and to look into the @todo
    items that I left.
    
    However, it is working in most cases. The only case that does not
    seem to work right now is that I seem to be using the same logic
    as #require when searching for a gem, so gems that do not put
    requirable files in the top level of their require paths will
    not work (e.g. minitest has to be required using minitest/unit).

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -25,8 +25,36 @@ class Gem::Commands::CompileCommand < Gem::Command
     "#{program_name} GEMNAME [GEMNAME ...]"
   end
 
+  ##
+  # Lookup the gems and their listed files. It will only compile files
+  # that are located in the `require_path` for a gem.
+
   def execute
-    puts options.inspect
+    specs = get_all_gem_names.map { |gem| Gem.searcher.find gem }.compact
+    if specs.empty?
+      message = "Did not find any of: #{get_all_gem_names.join ', '}"
+      raise ArgumentError, message
+    end
+
+    specs.each { |gem|
+      puts "Compiling #{gem.name}-#{gem.version}/"
+
+      lib_dirs  = Gem.searcher.lib_dirs_for gem
+      dirs      = Dir.glob lib_dirs
+      files     = dirs.map { |dir| Dir.glob( dir + '/**/*.rb' ) }.flatten
+      pp_regexp = Regexp.union dirs.map { |dir| dir.sub File.basename(dir), '' }
+
+      files.each { |file|
+        pp_file = file.sub pp_regexp, ''
+        puts "\t#{pp_file} => #{pp_file}o"
+
+        # @todo double check to see how macruby_deploy calls the compiler
+        `macrubyc -C '#{file}' -o '#{file}o'`
+        # @todo actually remove the file after I write some tests
+        puts 'removing original *.rb file now' if options[:'remove-original-files']
+      }
+    }
+
   end
 
 end
