6151b5499852d28cb6a1e0904f4d8362d396e87f
Mark Rada <mrada@marketcircle.com>
2011-02-25 00:17:20 -0500

    Remove the post-install hook
    It definitely wasn't using the proper API and its functionality
    will eventually be replaced with the new command (or an
    override for the install command).

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -63,23 +63,3 @@ class Gem::Commands::CompileCommand < Gem::Command
 end
 
 Gem::CommandManager.instance.register_command :compile
-
-
-Gem.post_install do |gem|
-
-  # @todo read option from .gemrc that would disable this hook
-
-  spec  = gem.spec
-  dir   = gem.gem_home + '/gems/' + spec.name + '-' + spec.version.version
-
-  files = spec.files - spec.test_files - spec.extra_rdoc_files
-  files = files.reject { |file| file.match /^(?:test|spec)/ }
-
-  files.each do |file|
-    next unless file.match /\.rb$/
-    puts "Compiling #{file} to #{file}o"
-    file = "#{dir}/#{file}"
-    `macrubyc -C '#{file}' -o '#{file}o'`
-  end
-
-end
