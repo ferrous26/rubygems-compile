6c827eb196a37631908a7ccca00bcc84c7f09056
Mark Rada <mrada@marketcircle.com>
2011-03-05 00:49:33 -0500

    Remove the --replace-original-files option since it doesn't work

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -4,30 +4,19 @@ require 'fileutils'
 require 'rbconfig'
 
 ##
-# @todo option to replace the original files, which implies removing
 
 class Gem::Commands::CompileCommand < Gem::Command
 
   MACRUBYC = File.join(RbConfig::CONFIG['bindir'], 'macrubyc')
 
   def initialize
-    defaults = {
-      :'remove-original-files'  => false,
-      :'replace-original-files' => false
-    }
+    defaults = { :'remove-original-files'  => false, }
     super 'compile', 'Compile gems using the MacRuby compiler', defaults
 
     add_option( '-r', '--[no-]remove-original-files',
                 'Delete the original *.rb source files after compiling',
                 ) do |value, options|
-      options[:'remove-original-files']  = value
-      options[:'replace-original-files'] = !value
-    end
-    add_option( '-R', '--[no-]replace-original-files',
-                'Replace the original *.rb files with their compiled version',
-                ) do |value, options|
-      options[:'replace-original-files'] = value
-      options[:'remove-original-files']  = !value
+      options[:'remove-original-files'] = value
     end
   end
 
@@ -51,14 +40,13 @@ class Gem::Commands::CompileCommand < Gem::Command
 
     verbose      = Gem.configuration.verbose
     slash        = verbose.is_a?(Fixnum) ? '/' : ''
-    extension    = 'o'
-    post_compile = Proc.new do |file| end
+    post_compile = Proc.new do |_,_| end
 
-    if options[:'replace-original-files']
-      extension = ''
-      post_compile = Proc.new do |file| FileUtils.mv "#{file}o", file; end
-    elsif options[:'remove-original-files']
-      post_compile = Proc.new do |file| FileUtils.rm file; end
+    if options[:'remove-original-files']
+      post_compile = Proc.new do |file, full_path|
+        say "\tRemoving #{file}" if verbose.is_a? Fixnum
+        FileUtils.rm full_path
+      end
     end
 
 
@@ -69,10 +57,10 @@ class Gem::Commands::CompileCommand < Gem::Command
       files = find_files_to_compile gem
 
       files.each { |file|
-        say "\t#{file} => #{file}#{extension}" if verbose.is_a? Fixnum
+        say "\t#{file} => #{file}o" if verbose.is_a? Fixnum
         full_path = path + '/' + file
         `#{MACRUBYC} -C '#{full_path}' -o '#{full_path}o'`
-        post_compile.call full_path
+        post_compile.call file, full_path
       }
     }
 
