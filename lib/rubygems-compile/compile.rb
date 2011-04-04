ddc753c3b46e2d993ff631ae825e54162e898188
Mark Rada <mrada@marketcircle.com>
2011-04-04 01:43:43 -0400

    Rewrite #execute to be less verbose

--- lib/rubygems-compile/compile.rb
+++ lib/rubygems-compile/compile.rb
@@ -35,23 +35,21 @@ class Gem::Commands::CompileCommand < Gem::Commands::InstallCommand
 
   def execute
 
-    verbose      = Gem.configuration.verbose
-    slash        = verbose.is_a?(Fixnum) ? '/' : ''
-    post_compile = Proc.new do |_,_| end
-    compile_options = { bundle: true }
-
+    post_compile = Proc.new { |_| }
     if options[:'remove-original-files']
-      post_compile = Proc.new do |file, full_path|
-        say "\tRemoving #{file}" if verbose.is_a? Fixnum
-        FileUtils.rm full_path
+      post_compile = Proc.new do |file|
+        say "\tRemoving #{File.basename(file)}" if Gem.configuration.really_verbose
+        FileUtils.rm file
       end
     end
 
     Gem.post_install do |gem|
       spec = gem.spec
-      say "Compiling #{spec.name}-#{spec.version}#{slash}" if verbose
 
-      path  = spec.full_gem_path
+      if Gem.configuration.verbose
+        slash = Gem.configuration.really_verbose ? '/' : ''
+        say "Compiling #{spec.name}-#{spec.version}#{slash}"
+      end
 
       files = spec.files - spec.test_files - spec.extra_rdoc_files
       files = files.reject do |file| file.match /^(?:test|spec)/ end
@@ -59,11 +57,14 @@ class Gem::Commands::CompileCommand < Gem::Commands::InstallCommand
       files = files.select do |file| file.match /\.rb$/ end
 
       files.each { |file|
-        say "\t#{file} => #{file}o" if verbose.is_a? Fixnum
-        full_path = path + '/' + file
-        compile_options.merge!( files: [full_path], output: "#{full_path}o" )
-        Compiler.new( compile_options ).run
-        post_compile.call file, full_path
+        say "\t#{file} => #{file}o" if Gem.configuration.really_verbose
+        full_path = File.join(spec.full_gem_path, file)
+        ::Compiler.new(
+                       bundle: true,
+                        files: [full_path],
+                       output: "#{full_path}o"
+                       ).run
+        post_compile.call(full_path)
       }
     end
 
