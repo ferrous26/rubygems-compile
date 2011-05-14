6bb0f6e286dd6d98366eebd09e1361a51361e592
Mark Rada <mrada@marketcircle.com>
2011-05-14 19:17:01 -0400

    Implement the auto_compile command

--- lib/rubygems-compile/commands/autocompile_command.rb
+++ lib/rubygems-compile/commands/autocompile_command.rb
@@ -1,14 +1,27 @@
 class Gem::Commands::AutoCompileCommand < Gem::Command
+  include Gem::UserInteraction
 
   def initialize
     super 'auto_compile', 'Enable gem compilation at install time'
   end
 
+  def description # :nodoc:
+    'Toggle a setting that enables compiling gems at install time.'
+  end
+
+  def usage
+    "#{progname}"
+  end
+
   def execute
-    # is on?
-    #   turn off
-    # is off?
-    #   turn on
+    Gem.configuration[:compile] = if Gem.configuration[:compile]
+                                    say 'Disabling automatic compilation'
+                                    false
+                                  else
+                                    say 'Enabling automatic compilation'
+                                    true
+                                  end
+    Gem.configuration.write
   end
 
 end
