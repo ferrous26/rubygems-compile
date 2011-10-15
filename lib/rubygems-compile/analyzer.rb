9ebb91129b090ee859095491cea5b86aadcaa67b
Mark Rada <mrada@marketcircle.com>
2011-10-14 23:34:55 -0400

    Better documentation for the analyzer class

--- lib/rubygems-compile/analyzer.rb
+++ lib/rubygems-compile/analyzer.rb
@@ -1,11 +1,20 @@
 require 'ripper'
 
+##
+# Uses the ripper parser to analyze ruby code contained in gems
+# and raises warnings if the code has potential issues.
+
 class Gem::Analyzer < Ripper::SexpBuilder
+
+  ##
+  # Raised in any case that the given code might display bad
+  # behaviours that were not intended.
 
   class Warning < Exception
   end
 
   def on_kw token
+    # Related to MacRuby ticket #721
     if token == '__FILE__'
       raise Warning, 'The __FILE__ keyword is used in this file'
     end
